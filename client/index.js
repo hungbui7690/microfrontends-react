5ad341acb8d3b185a913834ea9cff49a64122333
hungbui7690 <hungbui7690@gmail.com>
2023-03-26 14:51:20 +0700

    Project Setup

--- client/index.js
+++ client/index.js
@@ -1,22 +1,18 @@
 /*
-  What is a Microfrontend
-  - pic
-  - one single code base: monolithic SPA
-
-///////////////////////////////////////////
-
-  Application Overview
+  Project Setup  
   - pic
 
-///////////////////////////////////////////
+  > create products/ 
+    > npm init -y (create in products/)
 
-  Understanding Build-Time Integration
-  - pic
+  (***) need to install exact version of libraries 
 
-///////////////////////////////////////////
-
-  Runtime Integration 
-  - pic
-
+  > npm install faker@5.1.0 html-webpack-plugin@4.5.0 nodemon webpack@5.3.2 webpack-cli@4.1.0 webpack-dev-server@3.11.0
+    + faker@5.1.0
+    + nodemon
+    + webpack@5.3.2
+    + webpack-cli@4.1.0
+    + webpack-dev-server@3.11.0
+    + html-webpack-plugin@4.5.0
 
 */
