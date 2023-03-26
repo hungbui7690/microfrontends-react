236b28795d4313556d51e7b0d912faa3d75f990e
hungbui7690 <hungbui7690@gmail.com>
2023-03-26 14:45:49 +0700

    What is a Microfrontend & Overview

--- client/index.js
+++ client/index.js
@@ -1,4 +1,22 @@
 /*
-  Initial Setup
+  What is a Microfrontend
+  - pic
+  - one single code base: monolithic SPA
+
+///////////////////////////////////////////
+
+  Application Overview
+  - pic
+
+///////////////////////////////////////////
+
+  Understanding Build-Time Integration
+  - pic
+
+///////////////////////////////////////////
+
+  Runtime Integration 
+  - pic
+
 
 */
